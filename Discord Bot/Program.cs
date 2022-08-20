6af576cba49db5ede706a800d5692ee622ad92ca
Chris Garies <cgaries@dynamicsyntax.net>
2022-08-20 12:37:22 -0400

    Add project files.

---
+++ Discord Bot/Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+	public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+    public async Task MainAsync()
+	{
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var token = File.ReadAllText("token.txt");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
